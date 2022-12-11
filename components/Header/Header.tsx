6c451831e83ccc95830e9a78f6b402ee3872518b
Vladislav Pestsov <pestsov.js@gmail.com>
2022-12-11 14:00:18 +0200

    add header scroll

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import {ImSearch} from '@react-icons/all-files/im/ImSearch'
+import {BellIcon} from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export const Header = (): JSX.Element => {
     const categories = ["Home", "TV shows", "Movies", "New & Popular", "My List"]
 
+    const [isScrolled, setIsScrolled] = React.useState(false)
+
+    React.useEffect(() => {
+        const handleScroll = () => window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false)
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
     return (
-        <header>
+        <header className={`${isScrolled && "bg-[#141414]"}`}>
             <div className="flex items-center space-x-2 md:space-x-10">
                 <img alt=''
                     src="https://rb.gy/ulxxee"
@@ -19,8 +29,13 @@ export const Header = (): JSX.Element => {
                     )}
                 </ul>
             </div>
-            <div>
+            <div className="flex items-center space-x-4 text-sm font-light">
                 <ImSearch className="hidden sm:inline h-6 w-6" />
+                <p className="hidden lg:inline">Kids</p>
+                <BellIcon  className="h-6 w-6" />
+                <Link href="/account">
+                    <img src="https://rb.gy/g1pwyx" alt="" className="cursor-pointer rounded"/>
+                </Link>
             </div>
         </header>
     )
